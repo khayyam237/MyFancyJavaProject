2ef2594eb7241be46c7e45f020183791bb20cd2b
OmiRahman <momi183090@bscse.uiu.ac.bd>
2022-05-08 20:06:51 +0600

    initial commit

---
+++ MyFancyJavaProject.java
@@ -0,0 +1,18 @@
+
+package myfancyjavaproject;
+
+public class MyFancyJavaProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 62;
+        int b = 18;
+        int c = a+b;
+        // It will return the sum of two arguments.
+        System.out.println("The sum is =" + c);
+    }
+    
+}
