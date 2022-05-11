bbe42ec0fc367a02ddec306e5b827f6b35160f81
OmiRahman <momi183090@bscse.uiu.ac.bd>
2022-05-11 21:43:54 +0600

    2nd commit

--- MyFancyJavaProject.java
+++ MyFancyJavaProject.java
@@ -8,11 +8,11 @@ public class MyFancyJavaProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a = 62;
-        int b = 18;
-        int c = a+b;
+        int x = 62;
+        int y = 18;
+        int x = a+b;
         // It will return the sum of two arguments.
-        System.out.println("The sum is =" + c);
+        System.out.println("The sum of x+y is =" + x);
     }
     
 }
